4ffc599cbaa7817b235d3043087b4df498b98d63
quadribello <quadribello41@gmail.com>
2024-01-31 13:22:34 -0500

    added some output

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "test_policy_arn" {
+  value = aws_iam_role.oidc.arn
+}
+
+output "oidc-url" {
+  value = aws_iam_openid_connect_provider.eks.url
+}
+
+output "oidc-arn" {
+  value = aws_iam_openid_connect_provider.eks.arn
+}
+
+output "eks_cluster_autoscaler_arn" {
+  value = aws_iam_role.eks_cluster_autoscaler.arn
+}
